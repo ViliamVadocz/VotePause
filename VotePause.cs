71630076ff6611e89a015f774edc22df8ffd11d9
Viliam Vadocz <viliam.vadocz@gmail.com>
2025-04-06 15:42:10 +0200

    Add voting and feedback

--- VotePause.cs
+++ VotePause.cs
@@ -1,9 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using HarmonyLib;
 
 namespace VotePause;
 
+
 public class VotePause
 {
+    private static Dictionary<ulong, DateTime> pauseVotes = [];
+    private static Dictionary<ulong, DateTime> resumeVotes = [];
+
+    private const uint timeoutSeconds = 60;
+
+    private static uint playersNeeded(uint totalPlayers)
+    {
+        return (totalPlayers + 1) / 2;
+    }
 
     [HarmonyPatch(typeof(UIChatController), nameof(UIChatController.Event_Server_OnChatCommand))]
     public static class UIChatControllerEventServerOnChatCommandPatch
@@ -12,45 +25,68 @@ public class VotePause
         public static void Event_Server_OnChatCommand_Patch(
             Il2CppSystem.Collections.Generic.Dictionary<string, Il2CppSystem.Object> message)
         {
-            Plugin.Log.LogInfo($"Patch: Event_Server_OnChatCommand_Patch was called.");
-            if (message == null)
-            {
-                Plugin.Log.LogError("Received null message.");
-                return;
-            }
-
-            Plugin.Log.LogInfo($"message dict: {message.ToString()}");
-
-            ulong clientId = (ulong)Il2CppSystem.Convert.ToInt64(message["clientId"]);
-            Plugin.Log.LogInfo($"clientId: {clientId}");
-            string command = message["command"].ToString();
-            Plugin.Log.LogInfo($"command: {command}");
+            if (message == null) return;
+            if (!message.ContainsKey("command")) return;
+            if (!message.ContainsKey("clientId")) return;
 
             PlayerManager playerManager = NetworkBehaviourSingleton<PlayerManager>.Instance;
             GameManager gameManager = NetworkBehaviourSingleton<GameManager>.Instance;
+            UIChat uiChat = NetworkBehaviourSingleton<UIChat>.Instance;
 
-            if (gameManager == null)
-            {
-                Plugin.Log.LogError("Game Manager was null.");
-                return;
-            }
-            if (playerManager == null)
-            {
-                Plugin.Log.LogError("Player manager was null.");
-                return;
-            }
+            if (playerManager == null) return;
+            if (gameManager == null) return;
+            if (uiChat == null) return;
 
-            Player player = playerManager.GetPlayerByClientId(clientId);
-            Plugin.Log.LogInfo($"player: {player}");
-            if (command.Equals("/votepause") || command.Equals("/vp"))
+            uint totalPlayers = (uint)playerManager.GetPlayers(false).Count;
+            uint needed = playersNeeded(totalPlayers);
+
+            string command = message["command"].ToString().ToLower();
+            ulong clientId = (ulong)Il2CppSystem.Convert.ToInt64(message["clientId"]);
+            DateTime now = DateTime.UtcNow;
+
+            switch (command)
             {
-                Plugin.Log.LogInfo($"PAUSING!!!");
-                gameManager.Server_Pause();
-            }
-            else if (command.Equals("/voteresume") || command.Equals("/vr"))
-            {
-                Plugin.Log.LogInfo($"RESUMING!!!");
-                gameManager.Server_Resume();
+                case "/votepause":
+                case "/vp":
+                    pauseVotes[clientId] = now;
+                    pauseVotes = pauseVotes
+                        .Where(pair => now.Subtract(pair.Value).Seconds < timeoutSeconds)
+                        .ToDictionary(pair => pair.Key, pair => pair.Value);
+                    if (pauseVotes.Count >= needed)
+                    {
+                        uiChat.Server_SendSystemChatMessage($"<color=orange><b>VotePause</b></color> Vote passed - pausing! ({pauseVotes.Count}/{needed})");
+                        gameManager.Server_Pause();
+                        pauseVotes.Clear();
+                    }
+                    else
+                    {
+                        uiChat.Server_SendSystemChatMessage(
+                            $"<color=orange><b>VotePause</b></color> Vote to <b>pause</b> in progress ({pauseVotes.Count}/{needed})."
+                            + "Use <b>/votepause</b> or <b>/vp</b> to vote."
+                        );
+                    }
+                    break;
+                case "/voteresume":
+                case "/vr":
+                    resumeVotes[clientId] = now;
+                    resumeVotes = resumeVotes
+                        .Where(pair => now.Subtract(pair.Value).Seconds < timeoutSeconds)
+                        .ToDictionary(pair => pair.Key, pair => pair.Value);
+                    if (resumeVotes.Count >= needed)
+                    {
+                        uiChat.Server_SendSystemChatMessage($"<color=orange><b>VotePause</b></color> Vote passed - resuming! ({resumeVotes.Count}/{needed})");
+                        gameManager.Server_Resume();
+                        resumeVotes.Clear();
+                    }
+                    else
+                    {
+                        uiChat.Server_SendSystemChatMessage(
+                            $"<color=orange><b>VotePause</b></color> Vote to <b>resume</b> in progress ({resumeVotes.Count}/{needed})."
+                            + "Use <b>/voteresume</b> or <b>/vr</b> to vote."
+                        );
+                    }
+                    break;
+                default: return;
             }
         }
     }
