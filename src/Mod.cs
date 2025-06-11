82be86870135de1f0d41f811d8ceed734e2a7d70
Viliam Vadocz <viliam.vadocz@gmail.com>
2025-06-11 14:36:26 +0200

    Update for modding update

---
+++ src/Mod.cs
@@ -0,0 +1,64 @@
+using System;
+using HarmonyLib;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+namespace VotePause;
+
+public class Mod : IPuckMod
+{
+    public const string NAME = "VotePause";
+    public const string GUID = $"0x57696c6c.{NAME}";
+    public const string VERSION = "2.0.0";
+
+    private readonly Harmony harmony = new Harmony(GUID);
+
+    public bool OnEnable()
+    {
+        if (!IsDedicatedServer())
+        {
+            LogWarn("This is a server-side mod. It does nothing on the client.");
+            return false;
+        }
+        harmony.PatchAll();
+        LogPatchedMethods();
+        return true;
+    }
+
+    public bool OnDisable()
+    {
+        harmony.UnpatchSelf();
+        return true;
+    }
+
+    public static bool IsDedicatedServer()
+    {
+        return SystemInfo.graphicsDeviceType == GraphicsDeviceType.Null;
+    }
+
+    public void LogPatchedMethods()
+    {
+        LogDebug("Patched these methods:");
+        foreach (var method in harmony.GetPatchedMethods())
+        {
+            if (method.DeclaringType != null) LogDebug($" - {method.DeclaringType.FullName}.{method.Name}");
+        }
+    }
+
+    // Logging wrappers that prefix messages with the GUID.
+
+    public static void LogDebug(object message)
+    {
+        Debug.Log($"[{GUID}] {message}");
+    }
+
+    public static void LogWarn(object message)
+    {
+        Debug.LogWarning($"[{GUID}] {message}");
+    }
+
+    public static void LogError(object message)
+    {
+        Debug.LogError($"[{GUID}] {message}");
+    }
+}
--- src/VotePause.cs
+++ src/VotePause.cs
@@ -1,35 +1,47 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using HarmonyLib;
 
 namespace VotePause;
 
-
 public class VotePause
 {
-    const string messagePrefix = "<color=orange><b>VotePause</b></color>";
-    const string helpMessage = $"{messagePrefix} commands:\n"
+    private const string MESSAGE_PREFIX = $"<color=orange><b>{Mod.NAME}</b></color>";
+    private const string HELP_MESSAGE = $"commands:\n"
         + "* <b>/votepause</b> (/vp) - Vote to pause\n"
         + "* <b>/voteresume</b> (/vr) - Vote to resume";
 
+
+    private const uint TIMEOUT_SECONDS = 60;
     private static Dictionary<ulong, DateTime> pauseVotes = [];
     private static Dictionary<ulong, DateTime> resumeVotes = [];
-
-    private const uint timeoutSeconds = 60;
 
     private static uint PlayersNeeded(uint totalPlayers)
     {
         return (totalPlayers + 1) / 2;
     }
 
-    [HarmonyPatch(typeof(UIChatController), nameof(UIChatController.Event_Server_OnChatCommand))]
+    private static void sendMessage(UIChat uiChat, object message)
+    {
+        uiChat.Server_SendSystemChatMessage($"{MESSAGE_PREFIX} {message}");
+    }
+    private static void sendMessage(UIChat uiChat, object message, ulong clientId)
+    {
+        uiChat.Server_SendSystemChatMessage($"{MESSAGE_PREFIX} {message}", clientId);
+    }
+
+    [HarmonyPatch(typeof(UIChatController), "Event_Server_OnChatCommand")]
     public static class UIChatControllerEventServerOnChatCommandPatch
     {
+        static readonly FieldInfo _uiChat = typeof(UIChatController).GetField("uiChat", BindingFlags.Instance | BindingFlags.NonPublic);
+
         [HarmonyPrefix]
         public static void Event_Server_OnChatCommand(
             UIChatController __instance,
-            Il2CppSystem.Collections.Generic.Dictionary<string, Il2CppSystem.Object> message)
+            Dictionary<string, object> message
+        )
         {
             if (__instance == null) return;
             if (message == null) return;
@@ -38,7 +50,7 @@ public class VotePause
 
             PlayerManager playerManager = NetworkBehaviourSingleton<PlayerManager>.Instance;
             GameManager gameManager = NetworkBehaviourSingleton<GameManager>.Instance;
-            UIChat uiChat = __instance.uiChat;
+            UIChat uiChat = (UIChat)_uiChat.GetValue(__instance);
 
             if (playerManager == null) return;
             if (gameManager == null) return;
@@ -48,69 +60,69 @@ public class VotePause
             uint needed = PlayersNeeded(totalPlayers);
 
             string command = message["command"].ToString().ToLower();
-            ulong clientId = (ulong)Il2CppSystem.Convert.ToInt64(message["clientId"]);
+            ulong clientId = (ulong)message["clientId"];
             DateTime now = DateTime.UtcNow;
 
             switch (command)
             {
                 case "/help":
-                    uiChat.Server_SendSystemChatMessage(helpMessage, clientId);
+                    sendMessage(uiChat, HELP_MESSAGE, clientId);
                     break;
                 case "/votepause":
                 case "/vp":
-                    Plugin.Log.LogDebug($"ClientID {clientId} voted to pause at {now}.");
+                    Mod.LogDebug($"ClientID {clientId} voted to pause at {now}.");
                     pauseVotes = pauseVotes
-                        .Where(pair => now.Subtract(pair.Value).Seconds < timeoutSeconds)
+                        .Where(pair => now.Subtract(pair.Value).Seconds < TIMEOUT_SECONDS)
                         .ToDictionary(pair => pair.Key, pair => pair.Value);
                     bool alreadyVotedPause = pauseVotes.ContainsKey(clientId);
                     pauseVotes[clientId] = now;
                     if (pauseVotes.Count >= needed)
                     {
-                        Plugin.Log.LogDebug($"Vote to pause passed. [{pauseVotes.Count}/{needed}]");
-                        uiChat.Server_SendSystemChatMessage($"{messagePrefix} Vote passed - pausing! ({pauseVotes.Count}/{needed})");
+                        Mod.LogDebug($"Vote to pause passed. [{pauseVotes.Count}/{needed}]");
+                        sendMessage(uiChat, $" Vote passed - pausing! ({pauseVotes.Count}/{needed})");
                         gameManager.Server_Pause();
                         pauseVotes.Clear();
                     }
                     else if (!alreadyVotedPause)
                     {
-                        uiChat.Server_SendSystemChatMessage(
-                            $"{messagePrefix} Vote to <b>pause</b> in progress ({pauseVotes.Count}/{needed})."
+                        sendMessage(uiChat,
+                            $"Vote to <b>pause</b> in progress ({pauseVotes.Count}/{needed})."
                             + " Use <b>/votepause</b> or <b>/vp</b> to vote."
                         );
                     }
                     else
                     {
-                        Plugin.Log.LogDebug($"{clientId} tried to vote to pause but they already voted recently.");
-                        uiChat.Server_SendSystemChatMessage($"{messagePrefix} You already voted to <b>pause</b> recently.", clientId);
+                        Mod.LogDebug($"{clientId} tried to vote to pause but they already voted recently.");
+                        sendMessage(uiChat, $" You already voted to <b>pause</b> recently.", clientId);
                     }
                     break;
                 case "/voteresume":
                 case "/vr":
-                    Plugin.Log.LogDebug($"ClientID {clientId} voted to resume at {now}.");
+                    Mod.LogDebug($"ClientID {clientId} voted to resume at {now}.");
                     resumeVotes = resumeVotes
-                        .Where(pair => now.Subtract(pair.Value).Seconds < timeoutSeconds)
+                        .Where(pair => now.Subtract(pair.Value).Seconds < TIMEOUT_SECONDS)
                         .ToDictionary(pair => pair.Key, pair => pair.Value);
                     bool alreadyVotedResume = resumeVotes.ContainsKey(clientId);
                     resumeVotes[clientId] = now;
                     if (resumeVotes.Count >= needed)
                     {
-                        Plugin.Log.LogDebug($"Vote to resume passed. [{resumeVotes.Count}/{needed}]");
-                        uiChat.Server_SendSystemChatMessage($"{messagePrefix} Vote passed - resuming! ({resumeVotes.Count}/{needed})");
+                        Mod.LogDebug($"Vote to resume passed. [{resumeVotes.Count}/{needed}]");
+                        sendMessage(uiChat, $" Vote passed - resuming! ({resumeVotes.Count}/{needed})");
                         gameManager.Server_Resume();
                         resumeVotes.Clear();
                     }
                     else if (!alreadyVotedResume)
                     {
-                        Plugin.Log.LogDebug($"Vote to resume in progress. [{resumeVotes.Count}/{needed}]");
-                        uiChat.Server_SendSystemChatMessage(
-                            $"{messagePrefix} Vote to <b>resume</b> in progress ({resumeVotes.Count}/{needed})."
+                        Mod.LogDebug($"Vote to resume in progress. [{resumeVotes.Count}/{needed}]");
+                        sendMessage(uiChat,
+                            $"Vote to <b>resume</b> in progress ({resumeVotes.Count}/{needed})."
                             + " Use <b>/voteresume</b> or <b>/vr</b> to vote."
                         );
                     }
                     else
                     {
-                        Plugin.Log.LogDebug($"{clientId} tried to vote to resume but they already voted recently.");
-                        uiChat.Server_SendSystemChatMessage($"{messagePrefix} You already voted to <b>resume</b> recently.", clientId);
+                        Mod.LogDebug($"{clientId} tried to vote to resume but they already voted recently.");
+                        sendMessage(uiChat, $" You already voted to <b>resume</b> recently.", clientId);
                     }
                     break;
                 default: return;
